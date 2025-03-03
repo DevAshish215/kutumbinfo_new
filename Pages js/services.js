a6160eb4b2c76dc9eed52f6a16584af934f8f621
DevAshish215 <ashishpilare.dev@gmail.com>
2025-03-03 11:14:20 +0530

    Add files via upload

---
+++ Pages js/services.js
@@ -0,0 +1,44 @@
+// ============================
+// Fetch and Insert Navbar
+// ============================
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("../navbar.html") // Corrected Path
+        .then(res => res.text())
+        .then(data => document.getElementById("navbar-placeholder").innerHTML = data)
+        .catch(err => console.error("Error loading navbar:", err));
+});
+
+// ============================
+// Fetch and Insert Footer
+// ============================
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("../footer.html") // Corrected Path
+        .then(response => response.text())
+        .then(data => document.getElementById("footer-placeholder").innerHTML = data)
+        .catch(err => console.error("Error loading footer:", err));
+});
+
+//Navbar Links
+function handleWebDevelopmentClick() {
+    window.location.href = "../Pages/web_dev.html";
+}
+
+function handleMobileAppDevClick() {
+    window.location.href = "../Pages/mob_app.html";
+}
+
+function handleCustomSoftDevClick() {
+    window.location.href = "../Pages/custom_soft.html";
+}
+
+function handleUIUXDesignClick() {
+    window.location.href = "../Pages/ui_ux_design.html";
+}
+
+function handleAPIdevClick() {
+    window.location.href = "../Pages/api_dev.html";
+}
+
+function handleMaintainanceSupportClick() {
+    window.location.href = "../Pages/maintain_support.html";
+}
